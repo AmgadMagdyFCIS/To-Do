d70c7ad03a0e3436eb373fc7c45ccdaaa150d419
AnasKhaled1876 <Anas.khaled1892@gmail.com>
2020-09-15 03:56:00 +0200

    Navigation Drawer

--- app/src/main/java/com/example/to_do/activity/MainActivity.java
+++ app/src/main/java/com/example/to_do/activity/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -16,6 +19,7 @@ import com.example.to_do.fragments.MainFragment;
 
 public class MainActivity extends AppCompatActivity  {
 
+    private DrawerLayout drawer;
     Toolbar toolbar;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
@@ -26,6 +30,10 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         setSupportActionBar(toolbar);
 
         fragmentManager = getSupportFragmentManager();
@@ -36,4 +44,12 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
 }
